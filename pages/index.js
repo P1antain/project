2148120eb33f65f7a1eac1d042d6fa28c4f3d46a
P1antain <lifeg1ance@yandex.ru>
2021-05-09 16:40:34 +0300

    update adaptive header full

--- pages/index.js
+++ pages/index.js
@@ -9,3 +9,32 @@ btnBurger.addEventListener('click', ()=>{
         headerMain.classList.toggle('header__wrapper-main_active')
         headerIconLogin.classList.toggle('header__login_active')
 })
+
+const btnSearch = document.querySelector('.header__search')
+const headerSearch = document.querySelector('.header__search-field')
+
+btnSearch.addEventListener('click', ()=>{
+        headerSearch.classList.toggle('header__search-field_active')
+})
+
+const btnLogin = document.querySelector('.header__login')
+const popupLogin = document.querySelector('.popup__profile')
+
+btnLogin.addEventListener('click', ()=>{
+        popupLogin.classList.add('popup_active')
+})
+
+const btnClosePopup = document.querySelector('.popup__close')
+
+function closePopup(){
+        popupLogin.classList.remove('popup_active')
+}
+
+btnClosePopup.addEventListener('click', closePopup)
+
+const btnCity = document.querySelector('.header__burger-city')
+const popupCity = document.querySelector('.popup__city')
+
+btnCity.addEventListener('click', ()=>{
+        popupCity.classList.add('popup_active')
+})
